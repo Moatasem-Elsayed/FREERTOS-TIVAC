b99c707f1b6778198065d1bf1c073c8d540cc977
Moatasem El Sayed <66727825+Moatasem-Elsayed@users.noreply.github.com>
2020-12-15 22:46:32 +0200

    Add files via upload

---
+++ FREERTOS_LAB1/Debug/subdir_vars.mk
@@ -0,0 +1,91 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../tm4c1231h6pm.cmd 
+
+ASM_SRCS += \
+../portasm.asm 
+
+C_SRCS += \
+../DIO.c \
+../PLL.c \
+../heap_2.c \
+../list.c \
+../main.c \
+../port.c \
+../queue.c \
+../tasks.c \
+../tm4c1231h6pm_startup_ccs.c 
+
+C_DEPS += \
+./DIO.d \
+./PLL.d \
+./heap_2.d \
+./list.d \
+./main.d \
+./port.d \
+./queue.d \
+./tasks.d \
+./tm4c1231h6pm_startup_ccs.d 
+
+OBJS += \
+./DIO.obj \
+./PLL.obj \
+./heap_2.obj \
+./list.obj \
+./main.obj \
+./port.obj \
+./portasm.obj \
+./queue.obj \
+./tasks.obj \
+./tm4c1231h6pm_startup_ccs.obj 
+
+ASM_DEPS += \
+./portasm.d 
+
+OBJS__QUOTED += \
+"DIO.obj" \
+"PLL.obj" \
+"heap_2.obj" \
+"list.obj" \
+"main.obj" \
+"port.obj" \
+"portasm.obj" \
+"queue.obj" \
+"tasks.obj" \
+"tm4c1231h6pm_startup_ccs.obj" 
+
+C_DEPS__QUOTED += \
+"DIO.d" \
+"PLL.d" \
+"heap_2.d" \
+"list.d" \
+"main.d" \
+"port.d" \
+"queue.d" \
+"tasks.d" \
+"tm4c1231h6pm_startup_ccs.d" 
+
+ASM_DEPS__QUOTED += \
+"portasm.d" 
+
+C_SRCS__QUOTED += \
+"../DIO.c" \
+"../PLL.c" \
+"../heap_2.c" \
+"../list.c" \
+"../main.c" \
+"../port.c" \
+"../queue.c" \
+"../tasks.c" \
+"../tm4c1231h6pm_startup_ccs.c" 
+
+ASM_SRCS__QUOTED += \
+"../portasm.asm" 
+
+
